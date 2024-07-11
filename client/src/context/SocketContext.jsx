0048cb6edd146a2301f68c133c0b5939d5d1a4b7
Akgisno1 <anshulkumargodiwar2001@gmail.com>
2024-07-11 23:27:11 +0530

    third url change

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -9,11 +9,15 @@ export const SocketContextProvider = ({ children }) => {
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
-    setSocket(io("http://localhost:4000"));
+    setSocket(
+      io(
+        "https://rentifysocket-e7ko98qi4-anshul-kumar-godiwars-projects.vercel.app/"
+      )
+    );
   }, []);
 
   useEffect(() => {
-  currentUser && socket?.emit("newUser", currentUser.id);
+    currentUser && socket?.emit("newUser", currentUser.id);
   }, [currentUser, socket]);
 
   return (
