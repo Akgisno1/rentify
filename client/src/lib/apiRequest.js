97f1759b90d8511fe437c43b5978040f7ae04708
Akgisno1 <anshulkumargodiwar2001@gmail.com>
2024-07-13 00:54:22 +0530

    final final 2

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "https://rentifysocket.onrender.com",
+  baseURL: "https://rentifyapi-4wih.onrender.com",
   withCredentials: true,
 });
 
