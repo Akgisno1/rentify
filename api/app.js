66d7cdd86085d1dfd326dd186a729b8694371be1
Akgisno1 <anshulkumargodiwar2001@gmail.com>
2024-07-12 23:29:48 +0530

    new 1 final

--- api/app.js
+++ api/app.js
@@ -9,18 +9,19 @@ import chatRoute from "./routes/chat.route.js";
 import messageRoute from "./routes/message.route.js";
 
 const app = express();
+const port = process.env.PORT || 8800;
 
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/auth", authRoute);
-app.use("/api/users", userRoute);
-app.use("/api/posts", postRoute);
-app.use("/api/test", testRoute);
-app.use("/api/chats", chatRoute);
-app.use("/api/messages", messageRoute);
+app.use("/auth", authRoute);
+app.use("/users", userRoute);
+app.use("/posts", postRoute);
+app.use("/test", testRoute);
+app.use("/chats", chatRoute);
+app.use("/messages", messageRoute);
 
-app.listen(8800, () => {
+app.listen(port, () => {
   console.log("Server is running!");
 });
--- socket/app.js
+++ socket/app.js
@@ -37,5 +37,6 @@ io.on("connection", (socket) => {
     removeUser(socket.id);
   });
 });
+const port = process.env.PORT || 4000;
 
-io.listen("4000");
+io.listen(port);
